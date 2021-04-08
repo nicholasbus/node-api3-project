e84abee68c5ab931a056a622bb05a53e6e1177fb
Nicholas Smith <nicholassmithbus@gmail.com>
2021-04-08 14:24:04 -0600

    ready for deployment

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 require("dotenv").config();
 const server = require("./api/server");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 server.listen(port, () => console.log(`Server is listening on port ${port}`));
